4f090385e120fa3a227d63ac6b223721652597d2
ivolynets <ig.volynets@gmail.com>
2016-08-18 16:32:16 +0300

    Stack
    Added array stack implementation

--- data/collection/array_stack.go
+++ data/collection/array_stack.go
@@ -1,7 +1,7 @@
 package collection
 
 // Stack initial capacity
-const initCap = 8
+const initCap = 10
 
 // Array stack type
 type ArrayStack struct {
@@ -12,6 +12,57 @@ type ArrayStack struct {
 // Creates a new array stack instance and returns a pointer to it.
 func NewArrayStack() *ArrayStack {
 	return &ArrayStack{make([]interface{}, initCap), 0}
+}
+
+// Adds a new item at the top of the stack.
+func (s *ArrayStack) Push(i interface{}) {
+
+	s.extendStack(s.size + 1)
+	s.values[s.size] = i
+	s.size++
+
+}
+
+// Returns but does not remove an item at the top of the stack. If stack is not
+// empty than i contains value and ok is true. Otherwise i contains nil and ok
+// is false.
+func (s *ArrayStack) Peek() (i interface{}, ok bool) {
+
+	if ok = s.size > 0; ok {
+		i = s.values[s.size-1]
+	} else {
+		i = nil
+	}
+
+	return
+
+}
+
+// Removes and returns an item at the top of the stack. If stack is not empty
+// than i contains value and ok is true. Otherwise i contains nil and ok is
+// false.
+func (s *ArrayStack) Pop() (i interface{}, ok bool) {
+
+	if s.size > 0 {
+
+		s.size--
+		i = s.values[s.size]
+		s.values[s.size] = nil
+		s.compactStack()
+		ok = true
+
+	} else {
+		i = nil
+		ok = false
+	}
+
+	return
+
+}
+
+// Returns size of the stack.
+func (s *ArrayStack) Size() int {
+	return s.size
 }
 
 // Increases stack capacity if it's not enough.
@@ -33,12 +84,12 @@ func (s *ArrayStack) compactStack() {
 
 	if l := len(s.values); l > initCap {
 
-		c := s.size + (s.size >> 1)
+		c := l - l/3
 		if c < initCap {
 			c = initCap
 		}
 
-		if l-c >= l>>2 {
+		if c >= s.size {
 
 			d := make([]interface{}, c)
 			copy(d, s.values[:s.size])
---
+++ data/collection/array_stack_u_test.go
@@ -0,0 +1,230 @@
+package collection
+
+import "testing"
+
+// Test the stack internal extension function.
+func TestArrayStackExtend(t *testing.T) {
+
+	// init stack
+
+	lenExp := 4
+	capExp := 4
+
+	s := &ArrayStack{make([]interface{}, capExp), 0}
+
+	for i := 0; i < lenExp; i++ {
+		s.values[i] = i + 1
+		s.size++
+	}
+
+	// test if initial length of underlying array was not changed
+
+	testArrayStackAttributes(t, "Initial stack", s, lenExp, capExp)
+
+	// test how extension works
+
+	capExp = 6
+	s.extendStack(lenExp + 1)
+	testArrayStackAttributes(t, "Extended stack", s, lenExp, capExp)
+
+	// test if all items in the stack are still in place
+
+	for i := 0; i < s.size; i++ {
+		if s.values[i] != i+1 {
+			t.Fatalf("Extended stack: item %v not found", i+1)
+		}
+	}
+
+}
+
+// Test the stack internal compaction function.
+func TestArrayStackCompact(t *testing.T) {
+
+	// init stack
+
+	lenExp := initCap + 1
+	capExp := initCap + initCap>>1
+	capExp = capExp + capExp>>1
+
+	s := &ArrayStack{make([]interface{}, capExp), 0}
+
+	for i := 0; i < lenExp; i++ {
+		s.values[i] = i + 1
+		s.size++
+	}
+
+	// test if initial length of underlying array was not changed
+
+	testArrayStackAttributes(t, "Initial stack", s, lenExp, capExp)
+
+	// test how copaction works
+
+	capExp = initCap + initCap>>1
+	s.compactStack()
+	testArrayStackAttributes(t, "Compact stack", s, lenExp, capExp)
+
+	// test if all items in the stack are still in place
+
+	for i := 0; i < s.size; i++ {
+		if s.values[i] != i+1 {
+			t.Fatalf("Compacted stack: item %v not found", i+1)
+		}
+	}
+
+}
+
+// Tests the stack push function.
+func TestArrayStackPush(t *testing.T) {
+
+	s := NewArrayStack()
+
+	// test empty stack attributes
+
+	testArrayStackAttributes(t, "Empty stack", s, 0, initCap)
+
+	// test how push works
+
+	lenExp := initCap + 1
+	capExp := initCap + initCap>>1
+
+	for i := 1; i <= lenExp; i++ {
+		s.Push(i)
+	}
+
+	// test full stack attributes
+
+	testArrayStackAttributes(t, "Full stack", s, lenExp, capExp)
+
+}
+
+// Tests the stack peek function.
+func TestArrayStackPeek(t *testing.T) {
+
+	s := NewArrayStack()
+
+	// test how peek works with empty stack
+
+	i, ok := s.Peek()
+	testArrayStackResult(t, "Peek item from empty stack", i, nil, ok, false)
+	testArrayStackAttributes(t, "Peek item from empty stack", s, 0, initCap)
+
+	// init stack
+
+	lenExp := initCap + 1
+	capExp := initCap + initCap>>1
+
+	for i := 1; i <= lenExp; i++ {
+		s.Push(i)
+	}
+
+	// test how peek works with non-empty stack
+
+	i, ok = s.Peek()
+	testArrayStackResult(t, "Peek item from non-empty stack", i, lenExp, ok, true)
+	testArrayStackAttributes(t, "Peek item from non-empty stack", s, lenExp, capExp)
+
+}
+
+// Tests the stack pop function.
+func TestArrayStackPop(t *testing.T) {
+
+	s := NewArrayStack()
+
+	// init stack
+
+	lenExp := initCap
+	capExp := initCap
+
+	for i := 1; i <= lenExp; i++ {
+		s.Push(i)
+	}
+
+	// test how peek works with non-empty stack
+
+	for i := lenExp; i > 0; i-- {
+		v, ok := s.Pop()
+		lenExp--
+		testArrayStackResult(t, "Pop item from non-empty stack", v, i, ok, true)
+		testArrayStackAttributes(t, "Pop item from empty stack", s, lenExp, capExp)
+	}
+
+	// test how pop works with empty stack
+
+	i, ok := s.Pop()
+	testArrayStackResult(t, "Pop item from empty stack", i, nil, ok, false)
+	testArrayStackAttributes(t, "Pop item from empty stack", s, 0, capExp)
+
+}
+
+// Tests the stack size function.
+func TestArrayStackSize(t *testing.T) {
+
+	s := NewArrayStack()
+
+	// test empty stack size
+
+	if s.Size() != 0 {
+		t.Fatalf("Incorrect size of the stack, expected %v but was %v", 0, s.Size())
+	}
+
+	// init stack
+
+	lenExp := initCap
+
+	for i := 1; i <= lenExp; i++ {
+		s.Push(i)
+	}
+
+	// test non-empty stack size
+
+	if s.Size() != lenExp {
+		t.Fatalf("Incorrect size of the stack, expected %v but was %v", lenExp, s.Size())
+	}
+
+}
+
+// Tests stack attributes if they equal expected values.
+func testArrayStackAttributes(t *testing.T, m string, s *ArrayStack, sizeExp, capExp int) {
+
+	// test stack size
+
+	if s.size != sizeExp {
+		t.Fatalf("%s: incorrect size of the stack, expected %v but was %v", m, sizeExp, s.size)
+	}
+
+	// test stack underlying array length
+
+	lenAct := len(s.values)
+	if lenAct != capExp {
+		t.Fatalf("%s: incorrect length of the stack underlying array, expected %v but was %v", m, capExp, lenAct)
+	}
+
+	// test stack underlying array capacity
+
+	capAct := cap(s.values)
+	if capAct != capExp {
+		t.Fatalf("%s: incorrect capacity of the stack underlying array, expected %v but was %v", m, capExp, capAct)
+	}
+
+	// test stack underlying array values
+
+	for _, v := range s.values[s.size:] {
+		if v != nil {
+			t.Fatalf("%s: incorrect value of unused cell in the stack underlying array, expected %v but was %v", m, nil, v)
+		}
+	}
+
+}
+
+// Tests stack pop/peek function result values if they equal expected values.
+func testArrayStackResult(t *testing.T, m string, itemAct, itemExp interface{}, okAct, okExp bool) {
+
+	if okAct != okExp {
+		t.Fatalf("%s: OK flag value is incorrect, expected %v but was %v", m, okExp, okAct)
+	}
+
+	if itemAct != itemExp {
+		t.Fatalf("%s: value of item taken from stack is incorrect, expected %v but was %v", m, itemExp, itemAct)
+	}
+
+}
